4be14c52e66cfd3649e8d8cf7a20f75770b75a56
aleksandr@hp.notebook.win7 <burlakin.alexandr@yandex.ru>
2021-12-06 15:22:23 +0300

    Fix: Missing define 'EXPRDRAW_EXPORT'.

---
+++ src/function_name_expression.h
@@ -0,0 +1,20 @@
+#ifndef FUNCTIONNAMEEXPRESSION_H
+#define FUNCTIONNAMEEXPRESSION_H
+
+#include "simple_expression.h"
+
+namespace ExprDraw {
+
+class EXPRDRAW_EXPORT FunctionNameExpression : public SimpleExpression
+{
+public:
+  explicit FunctionNameExpression(const QString& text);
+
+protected:
+  // AbstractExpression interface
+  bool isArgNeedBrackets() const override;
+};
+
+} // namespace ExprDraw
+
+#endif // FUNCTIONNAMEEXPRESSION_H
